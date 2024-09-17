d976af2aec225d027b6063a14bd9b5761bd0de45
Kate Lynn Thomson <katelynn.jaremko@gmail.com>
2024-09-17 11:34:32 -0500

    exercise commit

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -8,229 +8,236 @@ namespace IterationStatements
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            //PrintPositiveToNegative(1000, -1000);
-            //PrintFactorThree();
-            //TwoEqualIntegers(3, 4);
-            //isOddOrEven();
-            //CanVote();
-            //IsInRange();
-        }
+        //LukeWarm Section: Create methods below
 
-        //    Console.WriteLine("Give me a number: ");
-        //    if (int.TryParse(Console.ReadLine(), out int baseNumber))
-        //    {
-        //        DisplayMultiplicationTable(baseNumber);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("The number you entered is not valid. Please input a number.");
-        //    }
-        //}
+        //Call the methods to test them in the Main method below 
 
-        /*LukeWarm Section: Create methods below
-
-        Call the methods to test them in the Main method below */
-
-        // 1) Write a method that will print to the console all numbers 1000 through - 1000
+        //1) Write a method that will print to the console all numbers 1000 through - 1000
 
         //Write a method that will print to the console all numbers 1000 through -1000.
-        //public static void PrintPositiveToNegative(int ceiling, int floor)
-        //{
-        //    for (int i = ceiling; i >= floor; i--)
-        //    {
-        //        Console.WriteLine(i);
-        //    }
-        //} 
+        public static void PrintPositiveToNegative(int ceiling, int floor)
+        {
+            for (int i = ceiling; i >= floor; i--)
+            {
+                Console.WriteLine(i);
+            }
+        }
 
-        // 2) Write a method that will print to the console numbers 3 through 999 by 3 each time.
+        //2) Write a method that will print to the console numbers 3 through 999 by 3 each time.
 
-        //public static void PrintFactorThree()
-        //{
-        //    for (int i = 3; i >= 3 && i <= 999; i++)
-        //    {
-        //        if (i % 3 == 0)
-        //            Console.WriteLine(i);
-        //    }
-        //}
+        public static void PrintFactorThree()
+        {
+            for (int i = 3; i >= 3 && i <= 999; i++)
+            {
+                    if (i % 3 == 0)
+                        Console.WriteLine(i);
+            }
+        }
 
-        // 3) Write a method to accept two integers as parameters and check whether they are equal or not
+        //3) Write a method to accept two integers as parameters and check whether they are equal or not
 
-        //public static bool twoequalintegers(int a, int b)
-        //{
-        //    // simple ternary
-        //    var isEqual = (a == b) ? true : false;
+        public static bool TwoEqualIntegers(int a, int b)
+        {
+                // simple ternary
+            var isEqual = (a == b) ? true : false;
 
-        //    Console.WriteLine(isEqual);
+            Console.WriteLine(isEqual);
 
-        //    return isEqual;
-        //}
+            return isEqual;
+        }
+        //4) Write a method to check whether a given number is even or odd 
 
-    //how do i determine if two integers are the same == 
-    //if (a == b)
-    //{
-    //    Console.WriteLine("Your integers are equal");
-    //}
-    //else
-    //{
-    //    Console.WriteLine("Your integers are for sure not equal");
-    //}
-    //}
+        public static void isOddOrEven()
+        {
+            var aNumber = 1;
+            if (aNumber == 0)
+            {
+                Console.WriteLine($"{aNumber} is 0.");
+            }
+            else if (aNumber % 2 == 0)
+            {
+                Console.WriteLine($"{aNumber} is even.");
+            }
+            else
+            {
+                Console.WriteLine($"{aNumber} is odd.");
+            }
+        }
 
-    // 4) Write a method to check whether a given number is even or odd 
+        //5) Write a method to check whether a given number is positive or negative
 
-    //public static void isOddOrEven()
-    //{
-    //    var aNumber = 1;
-    //    if (aNumber == 0)
-    //    {
-    //        Console.WriteLine($"{aNumber} is 0.");
-    //    }
-    //    else if (aNumber % 2 == 0)
-    //    {
-    //        Console.WriteLine($"{aNumber} is even.");
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine($"{aNumber} is odd.");
-    //    }
-    //}
+        public static void PositiveOrNegative(int checkNumber)
+        {
+            if (checkNumber != 0)
+            {
+                var message = (checkNumber > 0) ? $"{checkNumber} is positive" : $"{checkNumber} is negative";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine($"{checkNumber} is {checkNumber}");
+            }
+        }
+        // 6) Write a method to read the age of a candidate and determine whether they can vote.
+        //Hint: Use Parse or the safer TryParse() for an extra challenge 
 
-    // 5) Write a method to check whether a given number is positive or negative
+        public static void CanVote()
+        {
+            Console.WriteLine("Tell me your age.");
+            string? userInput = Console.ReadLine();
 
-    //public static void PositiveOrNegative(int checkNumber)
-    //{
-    //    if (checkNumber != 0)
-    //    {
-    //        var message = (checkNumber > 0) ? $"{checkNumber} is positive" : $"{checkNumber} is negative";
-    //        Console.WriteLine(message);
-    //    } 
-    //    else 
-    //    { 
-    //        Console.WriteLine($"{checkNumber} is {checkNumber}"); 
-    //    }
-    //}
+            if (int.TryParse(userInput, out int voterAge))
+            {
+                var message = (voterAge >= 18) ? "You can vote!" : "You cannot vote yet.";
+                Console.WriteLine(message);
+            }
+            else
+            {
+                Console.WriteLine("Invalid age input. Please enter a valid age.");
+            }
+        }
+        //Heatin Up Section:
+        //1) Write a method to check if an integer(from the user) is in the range -10 to 10 
 
-    /*6) Write a method to read the age of a candidate and determine whether they can vote.
-          Hint: Use Parse or the safer TryParse() for an extra challenge /*
+        public static void IsInRange()
+        {
+            Console.WriteLine("Give me a number between -10 and 10");
+            string? userInput = Console.ReadLine();
 
-    //public static void CanVote()
-    //{
-    //    Console.WriteLine("Tell me your age.");
-    //    string? userInput = Console.ReadLine();
+            if (int.TryParse(userInput, out int userNumber))
+            {
+                if (userNumber >= -10 && userNumber <= 10)
+                {
+                    Console.WriteLine($"Great job! {userNumber} is in between -10 and 10. You can follow directions.");
+                }
+                else
+                {
+                    Console.WriteLine($"{userNumber} is not in the correct range, try again later.");
+                }
+            }
+            else if (TryConvertWordToNumber(userInput, out int wordNumber))
+            {
+                if (wordNumber >= -10 && wordNumber <= 10)
+                {
+                    Console.WriteLine($"Great job! {wordNumber} is in between -10 and 10. You can follow directions.");
+                }
+                else
+                {
+                    Console.WriteLine($"{wordNumber} is not in the correct range, try again later.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Input. Please enter a valid number.");
+            }
+        }
 
-    //    if (int.TryParse(userInput, out int voterAge))
-    //    { 
-    //        var message = (voterAge >= 18) ? "You can vote!" : "You cannot vote yet.";
-    //        Console.WriteLine(message);
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine("Invalid age input. Please enter a valid age.");
-    //    }
-    //}
+        public static bool TryConvertWordToNumber(string? word, out int actualNumber)
+        {
+            if (word != null)
+            {
+                switch (word.ToLower())
+                {
+                    case "zero":
+                        actualNumber = 0;
+                        return true;
+                    case "one":
+                        actualNumber = 1;
+                        return true;
+                    case "two":
+                        actualNumber = 2;
+                        return true;
+                    case "three":
+                        actualNumber = 3;
+                        return true;
+                    case "four":
+                        actualNumber = 4;
+                        return true;
+                    case "five":
+                        actualNumber = 5;
+                        return true;
+                    case "six":
+                        actualNumber = 6;
+                        return true;
+                    case "seven":
+                        actualNumber = 7;
+                        return true;
+                    case "eight":
+                        actualNumber = 8;
+                        return true;
+                    case "nine":
+                        actualNumber = 9;
+                        return true;
+                    case "ten":
+                        actualNumber = 10;
+                        return true;
+                    default:
+                        actualNumber = int.MinValue;
+                        return false;
+                }
+            }
+            else
+            {
+                actualNumber = int.MinValue;
+                return false;
+            }
+        }
 
-    /*Heatin Up Section:
-    1) Write a method to check if an integer(from the user) is in the range -10 to 10 */
 
-    //public static void IsInRange()
-    //{
-    //    Console.WriteLine("Give me a number between -10 and 10");
-    //    string? userInput = Console.ReadLine();
+        // 2) Write a method to display the multiplication table(from 1 to 12) of a given integer
+        public static void DisplayMultiplicationTable(int baseNumber)
+        {
+            Console.WriteLine($"Multiplication table for {baseNumber}: ");
 
-    //    if (int.TryParse(userInput, out int userNumber))
-    //    {
-    //        if (userNumber >= -10 &&  userNumber <= 10) 
-    //        {
-    //            Console.WriteLine($"Great job! {userNumber} is in between -10 and 10. You can follow directions.");
-    //        }
-    //        else 
-    //        {
-    //            Console.WriteLine($"{userNumber} is not in the correct range, try again later.");
-    //        }
-    //    }
-    //    else if (TryConvertWordToNumber(userInput, out int wordNumber)) 
-    //    {
-    //        if (wordNumber >= -10 && wordNumber <= 10)
-    //        {
-    //            Console.WriteLine($"Great job! {wordNumber} is in between -10 and 10. You can follow directions."); 
-    //        }
-    //        else
-    //        {
-    //            Console.WriteLine($"{wordNumber} is not in the correct range, try again later.");
-    //        }
-    //    }
-    //    else
-    //    {
-    //        Console.WriteLine("Invalid Input. Please enter a valid number.");
-    //    } 
-    //}
+            for (int i = 1; i <= 12; i++)
+            {
+                int product = i * baseNumber;
+                Console.WriteLine($"{baseNumber} * {i} = {product}");
+            }
 
-    //public static bool TryConvertWordToNumber(string? word, out int actualNumber)
-    //{
-    //    if (word != null)
-    //    {
-    //        switch (word.ToLower())
-    //        {
-    //            case "zero":
-    //                actualNumber = 0;
-    //                return true;
-    //            case "one":
-    //                actualNumber = 1;
-    //                return true;
-    //            case "two":
-    //                actualNumber = 2;
-    //                return true;
-    //            case "three":
-    //                actualNumber = 3;
-    //                return true;
-    //            case "four":
-    //                actualNumber = 4;
-    //                return true;
-    //            case "five":
-    //                actualNumber = 5;
-    //                return true;
-    //            case "six":
-    //                actualNumber = 6;
-    //                return true;
-    //            case "seven":
-    //                actualNumber = 7;
-    //                return true;
-    //            case "eight":
-    //                actualNumber = 8;
-    //                return true;
-    //            case "nine":
-    //                actualNumber = 9;
-    //                return true;
-    //            case "ten":
-    //                actualNumber = 10;
-    //                return true;
-    //            default:
-    //                actualNumber = int.MinValue;
-    //                return false;
-    //        }
-    //    }
-    //    else
-    //    {
-    //        actualNumber = int.MinValue;
-    //        return false;
-    //    
+            //Console.WriteLine("Give me a number: ");
 
-    // 2) Write a method to display the multiplication table(from 1 to 12) of a given integer
-    //public static void DisplayMultiplicationTable(int baseNumber)
-    //{
-    //    Console.WriteLine($"Multiplication table for {baseNumber}: ");
+            //if (int.TryParse(Console.ReadLine(), out int baseNumber))
+            //{
+            //    DisplayMultiplicationTable(baseNumber);
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The number you entered is not valid. Please input a number.");
+            //}  
+        }
 
-    //    for (int i = 1; i <= 12; i++)
-    //    {
-    //        int product = i * baseNumber;
-    //        Console.WriteLine($"{baseNumber} * {i} = {product}");
-    //    }
-    //}
+        public static void Main(string[] args)
+        {
+            PrintPositiveToNegative(1000, -1000);
+            PrintFactorThree();
+            TwoEqualIntegers(3, 4);
+            isOddOrEven();
+            PositiveOrNegative(3);
+            CanVote();
+            IsInRange();
 
+            string[] words = { "three", "seven", "zero", "tree", null };
+
+            foreach (var word in words)
+            {
+                if (TryConvertWordToNumber(word, out int number))
+                {
+                    Console.WriteLine($"The word'{word}' converts to the number {number}.");
+                }
+                else
+                {
+                    Console.WriteLine($"The word '{word}' is not a valid number.");
+                }
+            }
+
+            DisplayMultiplicationTable(4);
+
+
+        }
+    }
 }
-}
+
 
 
 
